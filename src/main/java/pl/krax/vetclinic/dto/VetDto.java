f3a5ca944551d8aa37c93d0184d486a0ff01304b
KraxPL <kraxpl@gmail.com>
2023-04-19 13:02:10 +0200

    Vet adding and edition have validation now

--- src/main/java/pl/krax/vetclinic/dto/VetDto.java
+++ src/main/java/pl/krax/vetclinic/dto/VetDto.java
@@ -1,5 +1,8 @@
 package pl.krax.vetclinic.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +18,14 @@ import java.util.Set;
 @NoArgsConstructor
 public class VetDto {
     private Long id;
+    @NotNull
     private String degree;
+    @NotNull
     private String name;
+    @NotNull
+    @Email
     private String email;
     private List<Long> medicalHistoryIds;
+    @NotEmpty
     private Set<Role> roles;
 }
