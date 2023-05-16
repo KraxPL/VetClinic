ba9c11449444782d60674ac0afdab9161d6a904c
KraxPL <kraxpl@gmail.com>
2023-05-16 13:51:24 +0200

    Test for VetDailyScheduleService completed

--- src/test/java/pl/krax/vetclinic/service/impl/ChatMessageServiceImplTest.java
+++ src/test/java/pl/krax/vetclinic/service/impl/ChatMessageServiceImplTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ChatMessageServiceImplTest {
+class ChatMessageServiceImplTest {
 
     private ChatMessageServiceImpl chatMessageService;
 
