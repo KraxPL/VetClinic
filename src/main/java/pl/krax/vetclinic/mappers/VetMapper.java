325439d75b827e5640c9619ce51d83236d61ebe3
KraxPL <kraxpl@gmail.com>
2023-05-16 13:14:18 +0200

    For empty MedicalHistoryList mapper return empty list now

--- src/main/java/pl/krax/vetclinic/mappers/VetMapper.java
+++ src/main/java/pl/krax/vetclinic/mappers/VetMapper.java
@@ -9,18 +9,25 @@ import pl.krax.vetclinic.dto.VetDto;
 import pl.krax.vetclinic.entities.MedicalHistory;
 import pl.krax.vetclinic.entities.Vet;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface VetMapper {
     VetMapper VET_MAPPER = Mappers.getMapper(VetMapper.class);
+
     @Mapping(target = "medicalHistoryIds", source = "medicalHistoryList", qualifiedByName = "historyToHistoryIds")
     VetDto vetToDto(Vet vet);
+
     @Named("historyToHistoryIds")
-    default List<Long> medicalHistoryToMedicalHistoryIds(List<MedicalHistory> medicalHistory){
+    default List<Long> medicalHistoryToMedicalHistoryIds(List<MedicalHistory> medicalHistory) {
+        if (medicalHistory == null) {
+            return Collections.emptyList();
+        }
         return medicalHistory.stream()
                 .map(MedicalHistory::getId)
-                .toList();
+                .collect(Collectors.toList());
     }
 
     @Mapping(target = "medicalHistoryList", ignore = true)
